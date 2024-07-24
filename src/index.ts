b6e57a01d7310460811403e0bc43080c0167cf95
Perelyn <okohebina@gmail.com>
2024-07-24 13:06:10 +0100

    fixed client

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,34 @@
-export * from "./codegen";
+// export * from "./codegen";
 
 import { contracts } from "./codegen";
+import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
+import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
 
 const mnemonic =
   "tragic man unveil law index keep turtle aunt labor music virus today reflect immune print unveil farm uncover employ ocean leopard slab fix buyer";
 
-const cosmosPen = require("@cosmjs/sdk38");
-const cosmosStargate = require("@cosmjs/cosmwasm-stargate");
-const cosmosTendermint = require("@cosmjs/tendermint-rpc");
-const cosmosAmino = require("@cosmjs/amino");
-
 async function main() {
-  const pen = await cosmosPen.Secp256k1Pen.fromMnemonic(mnemonic);
+  // const pen = await cosmosPen.Secp256k1Pen.fromMnemonic(mnemonic);
 
-  const address = cosmosTendermint.pubkeyToAddress(
-    cosmosAmino.encodeSecp256k1Pubkey(pen.pubkey),
-    "xion",
-  );
-  console.log(address);
+  let wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
+    prefix: "xion",
+  });
 
-  const sc = new cosmosStargate.SigningCosmWasmClient(
+  const signingClient = await SigningCosmWasmClient.connectWithSigner(
     "https://rpc.xion-testnet-1.burnt.com/",
-    address,
-    (signBytes) => pen.sign(signBytes),
+    wallet,
   );
-  console.log(sc);
 
   const contractAddress =
     "xion1z7lyct6reu9gzsuq382ewn0jd3s4c85t6tl0z2gn5zq63px8feysxm5umq";
   const sender = "xion1vprmgc7066c9d9pfu4k0uq47v7lqz5tjk6f84m";
 
   const client = new contracts.Mintyplex.MintyplexClient(
-    sc,
+    signingClient,
     sender,
     contractAddress,
   );
+
+  console.log(client);
 }
 main();
